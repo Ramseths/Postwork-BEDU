4bab5dcd21a2ae04cee4c353c8a3ddf653e9713e
Ramseths <juan.ramseths@gmail.com>
2021-02-03 07:33:40 -0600

    Subiendo el postwork 7

---
+++ Postwork Sesion 7/Postwork 7.R
@@ -0,0 +1,18 @@
+#Postwork Sesion 7 28/01/2021
+#Jesus Ramseths Echeverria Rivera
+
+library(mongolite)
+
+
+match <- mongo(collection = "match", db="match_games",
+               url = "mongodb+srv://HDRAMSETHS:<password>@cluster0.pthh3.mongodb.net/test")
+
+match$count()
+
+match$find('{"HomeTeam":"Real Madrid","Date":"2015-12-20"}')
+
+match = mongo(collection = "mtcars", db = "match_games") 
+match$insert(mtcars)
+
+#Conexion Cerrada
+match$disconnect()
